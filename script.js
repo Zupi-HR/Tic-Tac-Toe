870ffbb2a7da552809a5512239409b2689621776
Zupi-HR <49796750+Zupi-HR@users.noreply.github.com>
2023-10-13 22:40:22 +0200

    doing step 4 with single array dimension

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
 const GameBoard = (function () {
   const gameboard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
+    null, null, null,
+    null, null, null,
+    null, null, null
   ];
 
-  function placeMarkAt(row, cell, player, playerMark) {
-    gameboard[row][cell] = playerMark;
 
+  function placeMarkAt(cell, player, playerMark) {
+    gameboard[cell] = playerMark;
   }
 
   return { gameboard, placeMarkAt };
 })();
 
-GameBoard.placeMarkAt(1, 0, 'Neven', 'O');
+
 
 
 const DisplayController = (function () {
   const GameBoardElement = document.querySelector('.gameboard');
   for (let i = 0; i < GameBoard.gameboard.length; i++) {
-    const row = document.createElement('div');
-    row.setAttribute('position', i);
-    row.classList.add('row');
-    GameBoardElement.append(row);
-    for (let j = 0; j < GameBoard.gameboard[i].length; j++) {
-      const cell = document.createElement('div');
-      cell.setAttribute('position', j);
-      cell.classList.add('cell');
-      cell.textContent = GameBoard.gameboard[i][j];
-      row.append(cell);
-    }
+    const cell = document.createElement('div');
+    cell.setAttribute('position', i);
+    cell.classList.add('cell');
+    cell.addEventListener('click', function() {
+      GameBoard.placeMarkAt(parseInt(cell.getAttribute('position')), 'SomePlayer', 'X');
+      cell.textContent = GameBoard.gameboard[parseInt(cell.getAttribute('position'))];
+    })
+    GameBoardElement.append(cell);
   }
 
 })();
